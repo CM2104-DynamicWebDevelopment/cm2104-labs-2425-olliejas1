435729047ffcd22fe3fb14da2e532222c0aab1c4
olliejas1 <o.jasnoch@rgu.ac.uk>
2025-01-30 10:50:23 +0000

    =3

--- lab_11/server.js
+++ lab_11/server.js
@@ -10,10 +10,11 @@ app.get('/test', function(req, res){
    });
 
 app.get('/add', function(req, res){
-    var x = req.query.x;
-    var y = req.query.y;
-     res.send("X + Y="+(x+y));
-    });
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+    res.send("X + Y = " + (x + y));
+});
+
 
 
 app.listen(8080);
