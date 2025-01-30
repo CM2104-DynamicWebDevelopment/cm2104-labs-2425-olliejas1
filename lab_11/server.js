eb95cfde25200c585a9fdb159904c792336d4b4b
olliejas1 <o.jasnoch@rgu.ac.uk>
2025-01-30 10:46:31 +0000

    Route 3

--- lab_11/server.js
+++ lab_11/server.js
@@ -9,5 +9,11 @@ app.get('/test', function(req, res){
     res.send("this is route 2");
    });
 
+app.get('/add', function(req, res){
+    var x = req.query.x;
+    var y = req.query.y;
+     res.send("X + Y="+(x+y));
+    });
+
 
 app.listen(8080);
