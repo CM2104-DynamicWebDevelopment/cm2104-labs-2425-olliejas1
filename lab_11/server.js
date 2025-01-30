864d59f8c52aecea809c19396963bb24f117e7e1
olliejas1 <o.jasnoch@rgu.ac.uk>
2025-01-30 10:59:40 +0000

    Calc

--- lab_11/server.js
+++ lab_11/server.js
@@ -15,6 +15,16 @@ app.get('/add', function(req, res){
     res.send("X + Y = " + (x + y));
 });
 
+app.get('/calc',function(req,res){
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y)
+    var operator = req.query.operator;
 
+    if (operator === "add") res.send("Result = " + (x+y));
+    else if (operator === "sub)") res.send("Result =" + (x-y));
+    else if (operator === "mul)") res.send("Result =" + (x*y));
+    else if (operator === "div)") res.send("Result =" + (x/y));
+    else res.send("Invalid Operator");
+})
 
 app.listen(8080);
